e822694033b739b158698be6113d29da92e6ee14
cedricongjx <150308512+cedricongjx@users.noreply.github.com>
2024-04-15 15:38:59 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,45 +1,35 @@
+
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class Question5 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter number of integers: ");
-        int num = in.nextInt();
-
-        // This array will hold the integers input by the user.
-        int[] arr = new int[num];
-
-        // Read integers from the user.
-        for (int i = 0; i < num; i++) {
-            arr[i] = in.nextInt();
-        }
-
-       
+        
+        int count = in.nextInt();
+        
         HashMap<Integer, Integer> frequencyMap = new HashMap<>();
-        for (int number : arr) {
-            if (frequencyMap.containsKey(number)) {
-                frequencyMap.put(number, frequencyMap.get(number) + 1);
-            } else {
-                frequencyMap.put(number, 1);
+        
+
+        for (int i = 0; i < count; i++) {
+            int num = in.nextInt();
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+        
+        
+        int mode = 0;
+        int maxFrequency = 0;
+        for (Integer key : frequencyMap.keySet()) {
+            if (frequencyMap.get(key) > maxFrequency) {
+                maxFrequency = frequencyMap.get(key);
+                mode = key;
             }
         }
-
-      
-        int mode = arr[0];  // Start with the first element.
-        int maxCount = frequencyMap.get(mode);
-
-        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
-            if (entry.getValue() > maxCount) {
-                maxCount = entry.getValue();
-                mode = entry.getKey();
-            }
-        }
-
-        // Printing the mode
+        
+     
         System.out.println(mode);
-
-        // Close the scanner
+        
+    
         in.close();
     }
 }
-
