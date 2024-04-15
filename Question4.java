ce75560801c3c3d6e38d1a941e8241f966daf1bf
cedricongjx <150308512+cedricongjx@users.noreply.github.com>
2024-04-15 15:26:49 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
 
-public class Question4
-{
-  public static void main(String[] args)
-  {
-    /**
-     * Prints a right angle triangle with *. The base of the triangle depends on the integer given.
-     *    e.g.
-     *     > 5
-     *     *****
-     *     ****
-     *     ***
-     *     **
-     *     *
-     * 
-     * Hint: 2 loops are required. System.out.print(...) to print on single line.
-     */
-     
-    Scanner in = new Scanner(System.in);
-    
-  }
+public class Question4 {
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n = Integer.parseInt(in.nextLine());
+
+        // Loop for each line of the triangle
+        for (int i = n; i > 0; i--) {
+            // Loop for printing the asterisks on each line
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            
+            System.out.println();
+        }
+
+        // Close the scanner
+        in.close();
+    }
 }
